da990c4a418f0cb0637c4b969a0bd4037c95364d
jvsampaios <70032581+jvsampaios@users.noreply.github.com>
2020-08-21 20:23:58 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,11 +1,31 @@
 function Enviar() {
 
-    var nome = document.getElementById("nomeid");
-    var email = document.getElementById("emailid");
-    var mensagem = document.getElementById("mensagemid");
-
-    if (nome.value != "" && email.value!="" && mensagem.value!="") {
-        alert('Obrigado sr(a) "' + nome.value + '" sua mensagem foi enviada e sera respondida pelo e-mail: ' + email.value);
+    var nome = form.nome.value;
+    var email = form.email.value;
+    var mensagem = form.mensagem.value;
+    if(form.nome.value=="" && form.email.value==""){
+        alert("Por favor, preencha um nome e um email válido.");
+        form.email.focus();
+        form.nome.focus();
+        return false;
+    }
+    else if(form.nome.value==""){
+        alert("Por favor, preencha um nome válido.");
+        form.nome.focus();
+        return false;
+    }
+     else if (email.indexOf("@") == -1 || email.indexOf(".") == -1){
+        alert("Por favor, preencha um e-mail válido.");
+        form.email.focus();
+        return false;
+    }
+    else if(form.mensagem.value==""){
+        alert("Por favor, preencha uma mensagem.");
+        form.mensagem.focus();
+        return false;
+    }
+    else if (nome.value != "" && mensagem.value!="") {
+        alert('Obrigado sr(a) "' + form.nome.value + '" sua mensagem foi enviada e sera respondida pelo e-mail: ' + form.email.value);
     }
 
-}s
+}
